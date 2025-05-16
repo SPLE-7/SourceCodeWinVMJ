f267737c4ad8012eb7f624163fdb7ff6fe59dd2c
Restu Ahmad Ar Ridho <restuaarridho@gmail.com>
2025-05-16 16:47:57 +0700

    feat: newtatatatata

--- modules/LibraryManagementSystem.buku.bukufisik/LibraryManagementSystem/buku/bukufisik/resource/BukuResourceImpl.java
+++ modules/LibraryManagementSystem.buku.bukufisik/LibraryManagementSystem/buku/bukufisik/resource/BukuResourceImpl.java
@@ -24,7 +24,6 @@ public class BukuResourceImpl extends BukuResourceDecorator {
 			return null;
 		}
 		return (bukuServiceImpl.createBukuFisik(vmjExchange.getPayload())).toHashMap();
-    return null;
 	}
 
 	// @Restriced(permission = "")
